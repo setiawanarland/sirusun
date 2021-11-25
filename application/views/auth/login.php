0e78f61f8f51ed1721e0537264dbbe6e56626396
setiawanarland <arlandmohammad@gmail.com>
2021-11-25 13:14:41 +0800

    mengubah tampilan login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Si Rusun Disperkimtan</title>
+    <title>SIRUSUN</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/assets-page/'); ?>img/jpt.png">
     <link href="<?= base_url('assets/'); ?>css/style.css" rel="stylesheet">
@@ -13,38 +13,36 @@
 </head>
 
 <body class="h-100">
-    <div class="authincation h-100">
-        <div class="container-fluid h-100">
-            <div class="row justify-content-center h-100 align-items-center">
-                <div class="col-md-6">
-                    <div class="authincation-content">
-                        <div class="row no-gutters">
-                            <div class="col-xl-12">
-                                <div class="auth-form">
-                                    <h4 class="text-center mb-4">Sign in your account</h4>
-                                    <?= $this->session->flashdata('message'); ?>
-                                    <form action="" method="post">
-                                        <div class="form-group">
-                                            <label><strong>Username</strong></label>
-                                            <input type="text" id="username" name="username" class="form-control" value="<?= set_value('username'); ?>">
-                                            <?= form_error('username', '<small class="text-danger">', '</small>'); ?>
-                                        </div>
-                                        <div class="form-group">
-                                            <label><strong>Password</strong></label>
-                                            <input type="password" id="password" name="password" class="form-control">
-                                        </div>
-                                        <div class="form-row d-flex justify-content-between mt-4 mb-2">
+    <div class="authincation h-100 bg">
+        <div class="overlay">
+            <div class="container-fluid h-100">
+                <div class="row justify-content-center h-100 align-items-center">
+                    <div class="col-md-4">
+                        <div class="authincation-content">
+                            <div class="row no-gutters">
+                                <div class="col-xl-12">
+                                    <div class="auth-form">
+                                        <img src="<?= base_url('assets/assets-page/'); ?>img/jpt.png" alt="" width="40%" class="figure-img img-fluid rounded mx-auto d-block">
+                                        <h4 class="text-center text-white mb-4">SIRUSUN <br> Disperkimtan Kab. Jeneponto</h4>
+                                        <?= $this->session->flashdata('message'); ?>
+                                        <form action="" method="post">
                                             <div class="form-group">
-                                                <a href="#">Forgot Password?</a>
+                                                <label class="text-white">Username</label>
+                                                <input type="text" id="username" name="username" class="form-control" value="<?= set_value('username'); ?>" autofocus>
+                                                <?= form_error('username', '<small class="text-danger">', '</small>'); ?>
                                             </div>
+                                            <div class="form-group">
+                                                <label class="text-white">Password</label>
+                                                <input type="password" id="password" name="password" class="form-control">
+                                            </div>
+                                            <div class="text-center">
+                                                <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                                            </div>
+                                        </form>
+                                        <div class="text-center mt-4">
+                                            <label class="text-white text-center">Copyright @2021 Disperkimtan Kab. Jeneponto. <br> All rights reserved.</label>
                                         </div>
-                                        <div class="text-center">
-                                            <button type="submit" class="btn btn-primary btn-block">Sign me in</button>
-                                        </div>
-                                    </form>
-                                    <!-- <div class="new-account mt-3">
-                                        <p>Don't have an account? <a class="text-primary" href="<?= base_url('auth/registration'); ?>">Sign up</a></p>
-                                    </div> -->
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Si Rusun Disperkimtan Jeneponto </title>
+    <title>SIRUSUN</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/assets-page/'); ?>img/jpt.png">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendor/owl-carousel/css/owl.carousel.min.css">
